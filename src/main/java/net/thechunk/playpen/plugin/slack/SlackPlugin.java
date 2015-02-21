145de8f07d3e7f0cf6519291802c88d7d9ebf5af
Sam Bloomberg <sam@redxdev.com>
2015-02-21 15:58:34 -0500

    provision and deprovision commands

--- src/main/java/net/thechunk/playpen/plugin/slack/SlackPlugin.java
+++ src/main/java/net/thechunk/playpen/plugin/slack/SlackPlugin.java
@@ -5,12 +5,16 @@ import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 import lombok.extern.log4j.Log4j2;
 import net.thechunk.playpen.coordinator.CoordinatorMode;
 import net.thechunk.playpen.coordinator.PlayPen;
-import net.thechunk.playpen.coordinator.network.INetworkListener;
-import net.thechunk.playpen.coordinator.network.LocalCoordinator;
-import net.thechunk.playpen.coordinator.network.Network;
-import net.thechunk.playpen.coordinator.network.Server;
+import net.thechunk.playpen.coordinator.network.*;
+import net.thechunk.playpen.p3.P3Package;
 import net.thechunk.playpen.plugin.AbstractPlugin;
 import net.thechunk.playpen.plugin.EventManager;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 @Log4j2
 public class SlackPlugin extends AbstractPlugin implements INetworkListener, SlackMessageListener {
@@ -138,11 +142,19 @@ public class SlackPlugin extends AbstractPlugin implements INetworkListener, Sla
 
                 case "help":
                     sendMessage("Available commands:\n" +
-                            "help, list");
+                            "help, list, provision, deprovision");
                     break;
 
                 case "list":
                     runListCommand(args);
+                    break;
+
+                case "provision":
+                    runProvisionCommand(args);
+                    break;
+
+                case "deprovision":
+                    runDeprovisionCommand(args);
                     break;
             }
         }
@@ -185,5 +197,127 @@ public class SlackPlugin extends AbstractPlugin implements INetworkListener, Sla
         }
 
         sendMessage(result);
+    }
+
+    private void runProvisionCommand(String[] args) {
+        if(args.length < 3) {
+            sendMessage("Usage: @playpen provision <package-id> [properties...]\n" +
+                    "Provisions a server on the network.\n" +
+                    "The property 'version' will specify the version of the package (default: promoted)\n" +
+                    "The property 'coordinator' will specify which coordinator to provision on (default: best fit)\n" +
+                    "The property 'name' will specify the name of the server.");
+            return;
+        }
+
+        String id = args[2];
+        String version = "promoted";
+        String coordinator = null;
+        String serverName = null;
+        Map<String, String> properties = new HashMap<>();
+        for(int i = 3; i < args.length; i += 2) {
+            if(i + 1 >= args.length) {
+                sendMessage("Properties must be in the form <key> <value>");
+                return;
+            }
+
+            String key = args[i];
+            String value = args[i+1];
+
+            String lowerKey = key.trim().toLowerCase();
+            switch(lowerKey) {
+                case "version":
+                    version = value;
+                    break;
+
+                case "coordinator":
+                    coordinator = value;
+                    break;
+
+                case "name":
+                    serverName = value;
+                    break;
+
+                default:
+                    properties.put(key, value);
+                    break;
+            }
+        }
+
+        P3Package p3 = Network.get().getPackageManager().resolve(id, version);
+        if(p3 == null) {
+            sendMessage("Unable to resolve package " + id + "(" + version + ")");
+            return;
+        }
+
+        ProvisionResult result;
+        if(coordinator == null) {
+            result = Network.get().provision(p3, serverName, properties);
+        }
+        else {
+            result = Network.get().provision(p3, serverName, properties, coordinator);
+        }
+
+        if(result == null) {
+            sendMessage("Unable to provision server.");
+            return;
+        }
+
+        sendMessage("Provision request successful.\n" +
+                "  Coordinator uuid: " + result.getCoordinator() + "\n" +
+                "  Server uuid: " + result.getServer());
+    }
+
+    private void runDeprovisionCommand(String[] args) {
+        if(args.length != 4 && args.length != 5) {
+            sendMessage("Usage: @playpen deprovision <coordinator> <server> [force=false]\n" +
+                    "Deprovisions a server from the network. Coordinator and server arguments accept regex.\n" +
+                    "FOr safety, all regex will have ^ prepended and $ appended.");
+            return;
+        }
+
+        Pattern coordPattern = Pattern.compile('^' + args[2] + '$');
+        Pattern serverPattern = Pattern.compile('^' + args[3] + '$');
+        boolean force = args.length == 5 && (args[4].trim().toLowerCase().equals("true"));
+
+        if(force)
+            sendMessage("Note: deprovisioning via force");
+
+        sendMessage("One moment please...");
+
+        Map<String, List<String>> servers = new HashMap<>();
+        for(LocalCoordinator coord : Network.get().getCoordinators().values()) {
+            if(coordPattern.matcher(coord.getUuid()).matches() ||
+                    (coord.getName() != null && coordPattern.matcher(coord.getName()).matches())) {
+                List<String> serverList = new LinkedList<>();
+                for(Server server : coord.getServers().values()) {
+                    if(serverPattern.matcher(server.getUuid()).matches() ||
+                            (server.getName() != null && serverPattern.matcher(server.getName()).matches())) {
+                        serverList.add(server.getUuid());
+                    }
+                }
+
+                if(!servers.isEmpty())
+                    servers.put(coord.getUuid(), serverList);
+            }
+        }
+
+        if(servers.isEmpty()) {
+            sendMessage("I couldn't find any servers to deprovision matching those patterns.");
+            return;
+        }
+
+        for(Map.Entry<String, List<String>> entry : servers.entrySet()) {
+            String coord = entry.getKey();
+            for(String server : entry.getValue()) {
+                if(Network.get().deprovision(coord, server, force)) {
+                    sendMessage("Sent deprovision for " + server + " on coordinator " + coord);
+                }
+                else {
+                    sendMessage("Unable to send deprovision for " + server + " on coordinator " + coord);
+                }
+            }
+        }
+
+        sendMessage("Deprovision operation complete!");
     }
 }
